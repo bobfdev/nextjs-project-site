e819ae88884e997ec9156baf39417292769d9e2c
Bob F <bobfdev@gmail.com>
2022-01-23 22:04:08 -0600

    initial files

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import Nav from './Nav';
+import styles from '../styles/Layout.module.css'
+
+function Layout({ children }) {
+  return (
+    <>
+        <Nav />
+        <div className={styles.container}>
+            <main className={styles.main}>{children}</main>
+        </div>
+    </>
+  );
+}
+
+export default Layout;
